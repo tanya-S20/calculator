26448b0b49ec96dc6aad122230feec51a5daf3a1
tanya-S20 <142837716+tanya-S20@users.noreply.github.com>
2023-08-28 21:36:10 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    float num1, num2;
+
+    cout<<  "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Error: Division by zero is not allowed." << endl;
+            break;
+        default:
+            cout << "Error: Invalid operator." << endl;
+    }
+
+ return 0;
+}
